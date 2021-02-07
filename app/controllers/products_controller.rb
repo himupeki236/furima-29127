612b8c5d86ebb8f123e3a2b79fd11f7b1f2deda8
himupeki236 <usodatoitteyojoe@gmail.com>
2021-02-07 15:40:37 +0900

    商品削除機能_指摘修正No.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,8 +43,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product.destroy
-    redirect_to root_path
+    if current_user.id = @product.id
+      @product.destroy
+      redirect_to root_path
+    end
   end
 
   private
